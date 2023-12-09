8ab9609c3726f95ebc8c3a8007edecd6f660a60a
ela1ter <3214sergy@gmail.com>
2023-12-09 14:56:52 +0300

    tg

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-bot= Bot(token="6551460718:AAGWaCb35zbQ62aPV2KWviHJz-OVMQOdfiU")
-dispatcher = Dispatcher(bot)
-@dispatcher.message_handler()
-async def handle_message(message: types.Message):
-    await message.answer(message.text)
-executor.start_polling(dispatcher, skip_updates=True)
+import asyncio
+import logging
+import sys
+import requests
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+dispatcher = Dispatcher()
+bot = Bot("6551460718:AAGWaCb35zbQ62aPV2KWviHJz-OVMQOdfiU")
+
+@dispatcher.message(CommandStart())
+async def command_start_handler(message: Message):
+    catButton = [
+        [
+            types.KeyboardButton(text='Хочу котика!')
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=catButton,
+        resize_keyboard = True,
+        input_field_placeholder='Котики'
+    )
+    await message.answer(text 'Хочу котика!', reply_markup=keyboard)
+@dispatcher.message(F.text == "Хочу котика!")
+async def image_handler(message: Message):
+    responce = requests.get('https://api.thecatapi.com/v1/images/search').json()
+    await message.aswer_photo(responce[0]['url'])
+async def main():
+    await dispatcher.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
